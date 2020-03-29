20b1673cfea8cdefb39cffdfc6a112742db99d2c
Vinit <vinit.x.kumar@tsgforce.com>
2020-03-29 18:57:17 +0800

    Microservices

---
+++ gateway/Readme.md
@@ -0,0 +1,5 @@
+http://localhost:8095/get
+
+Hystrix:
+curl --dump-header - --header 'Host: www.hystrix.com' http://localhost:8095/delay/3
+
--- gateway/src/main/java/gateway/config/Configurations.java
+++ gateway/src/main/java/gateway/config/Configurations.java
@@ -8,12 +8,28 @@ import org.springframework.context.annotation.Bean;
 @SpringBootConfiguration
 public class Configurations {
 
+    //@Bean
+    public RouteLocator myRoutesXXX(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
                         .path("/get")
                         .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f.hystrix(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
                         .uri("http://httpbin.org:80"))
                 .build();
     }
